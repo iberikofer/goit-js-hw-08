7d2f3b0e18e202285cff8b7f9eea0293ddd7607b
Yaroslav <sych521@gmail.com>
2023-04-04 19:33:35 +0300

    default

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.getElementsByClassName('gallery')[0];
+
+function doMarkup(array) {
+  return array
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+				<a class="gallery__link" href="${original}">
+      		<img class="gallery__image" src="${preview}" alt="${description}" />
+  			</a>
+			</li>
+        `;
+    })
+    .join('');
+}
+
+function replaceMarkupToDoc(markupString) {
+  galleryList.innerHTML = markupString;
+}
+
+replaceMarkupToDoc(doMarkup(galleryItems));
+
+new SimpleLightbox('.gallery a', {
+  captionsData: `alt`,
+  captionDelay: 250,
+});
