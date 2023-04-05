e3abcefb488f574db6a0278a7132ae9a35cd2143
Yaroslav <sych521@gmail.com>
2023-04-05 16:44:12 +0300

    Second task draft

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+import Vimeo from '@vimeo/player';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo(iframe);
 
+player.play();
+player.on('timeupdate', onTimeUpdate);
+
+function onTimeUpdate(time) {
+  console.log(time);
+}
