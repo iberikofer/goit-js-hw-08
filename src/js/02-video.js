2666b85d2bd7f0ab1a00f603c234343e38a47db2
Yaroslav <sych521@gmail.com>
2023-04-05 21:02:13 +0300

    Second task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,19 @@
-import Vimeo from '@vimeo/player';
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
+
 const iframe = document.querySelector('#vimeo-player');
-const player = new Vimeo(iframe);
+const player = new Player(iframe);
 
-player.play();
-player.on('timeupdate', onTimeUpdate);
+player.on('timeupdate', throttle(onPlay, 1000));
 
-function onTimeUpdate(time) {
-  console.log(time);
+function onPlay(time) {
+  localStorage.setItem('videoplayer-current-time', time.seconds);
+}
+
+const lastPayerTime = localStorage.getItem('videoplayer-current-time');
+
+if (lastPayerTime) {
+  player.setCurrentTime(lastPayerTime);
+} else {
+  console.log('=)');
 }
